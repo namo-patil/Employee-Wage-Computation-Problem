c6ea855993e60e5660815319d11a1788a76f7502
namo-patil <namratapatil74738@gmail.com>
2022-04-20 16:10:30 +0530

    Modified: Replaced if-else statement with switch statement

--- src/com/bridgelabz/empwagecomputationprograms/EmployeeWage.java
+++ src/com/bridgelabz/empwagecomputationprograms/EmployeeWage.java
@@ -3,24 +3,26 @@ package com.bridgelabz.empwagecomputationprograms;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int IS_FULL_TIME = 2;
-        int IS_PART_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
+        int Is_Full_Time = 2;
+        int Is_Part_Time = 1;
+        int Emp_Rate_Per_Hour = 20;
         int empHrs = 0;
         int empCheck = (int) (Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present, Full day");
-            empHrs = 8;
+
+        switch (empCheck) {
+            case 1:
+                empHrs = 4;
+                System.out.println("Employee is Working as Part Time");
+                break;
+            case 2:
+                empHrs = 8;
+                System.out.println("Employee is Present Full Time");
+                break;
+            default:
+                System.out.println("Employee is Absent");
+                empHrs = 0;
         }
-        else if (empCheck == IS_PART_TIME) {
-            System.out.println("Employee is Present, Half day");
-            empHrs = 4;
-        }
-        else {
-            System.out.println("Employee is Absent");
-            empHrs = 0;
-        }
-        int dailyEmpWage = EMP_RATE_PER_HOUR * empHrs;
+        int dailyEmpWage = Emp_Rate_Per_Hour * empHrs;
         System.out.println("Daily Employee Wage : " + dailyEmpWage);
     }
 }
