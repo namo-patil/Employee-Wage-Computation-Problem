693c0cad87322660c273e14cb3cd6f9948231762
namo-patil <namratapatil74738@gmail.com>
2022-04-20 17:40:30 +0530

    Added: Program to calculate employee wage till a condition of total working hours or days is reached for a month

--- src/com/bridgelabz/empwagecomputationprograms/EmployeeWage.java
+++ src/com/bridgelabz/empwagecomputationprograms/EmployeeWage.java
@@ -5,30 +5,31 @@ public class EmployeeWage {
     public static final int IS_PART_TIME = 1;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int WORKING_DAYS_PER_MONTH = 20;
+    public static final int MAX_WORKING_HRS_PER_MONTH = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int empHrs = 0;
         int monthlyEmpWage = 0;
-        for (int i = 1; i <= WORKING_DAYS_PER_MONTH; i++) {
+        int totalWorkingDays = 0;
+        int totalWorkingHrs = 0;
+        while (totalWorkingHrs <= MAX_WORKING_HRS_PER_MONTH && totalWorkingDays < WORKING_DAYS_PER_MONTH) {
+            totalWorkingDays++;
             int empCheck = (int) (Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_FULL_TIME:
                     empHrs = 8;
-                    System.out.println("Employee is Present, Full Time");
                     break;
                 case IS_PART_TIME:
                     empHrs = 4;
-                    System.out.println("Employee is Present, Part Time");
                     break;
                 default:
-                    System.out.println("Employee is Absent");
                     empHrs = 0;
             }
-            int dailyEmpWage = EMP_RATE_PER_HOUR * empHrs;
-            monthlyEmpWage += dailyEmpWage;
-            System.out.println("Daily Employee Wage : " + dailyEmpWage);
+            totalWorkingHrs += empHrs;
+            System.out.println("#DAY : " + totalWorkingDays + " , #Total Working Hours : " + totalWorkingHrs);
         }
+        monthlyEmpWage = totalWorkingHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage for a Month: " + monthlyEmpWage);
     }
 }
