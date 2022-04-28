319ef9d1291a83953e9795621a7abea96203be26
namo-patil <namratapatil74738@gmail.com>
2022-04-28 19:26:25 +0530

    Added: Code to calculate employee wage for multiple companies

--- src/com/bridgelabz/empwagecomputationprograms/EmployeeWage.java
+++ src/com/bridgelabz/empwagecomputationprograms/EmployeeWage.java
@@ -3,17 +3,18 @@ package com.bridgelabz.empwagecomputationprograms;
 public class EmployeeWage {
     public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int WORKING_DAYS_PER_MONTH = 20;
-    public static final int MAX_WORKING_HRS_PER_MONTH = 100;
+    //public static final int EMP_RATE_PER_HOUR = 20;
+    //public static final int WORKING_DAYS_PER_MONTH = 20;
+    //public static final int MAX_WORKING_HRS_PER_MONTH = 100;
 
-    public static int computeEmpWage() {
+    public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDaysPerMonth,
+                                     int maxWorkingHoursPerMonth) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int empHrs = 0;
         int monthlyEmpWage = 0;
         int totalWorkingDays = 0;
         int totalWorkingHrs = 0;
-        while (totalWorkingHrs <= MAX_WORKING_HRS_PER_MONTH && totalWorkingDays < WORKING_DAYS_PER_MONTH) {
+        while (totalWorkingHrs <= maxWorkingHoursPerMonth && totalWorkingDays < numOfWorkingDaysPerMonth) {
             totalWorkingDays++;
             int empCheck = (int) (Math.random() * 10) % 3;
             switch (empCheck) {
@@ -29,12 +30,16 @@ public class EmployeeWage {
             totalWorkingHrs += empHrs;
             System.out.println("#DAY : " + totalWorkingDays + " , #Total Working Hours : " + totalWorkingHrs);
         }
-        monthlyEmpWage = totalWorkingHrs * EMP_RATE_PER_HOUR;
+        monthlyEmpWage = totalWorkingHrs * empRatePerHour;
         System.out.println("Employee Wage for a Month: " + monthlyEmpWage);
         return monthlyEmpWage;
     }
 
     public static void main(String[] args) {
-        computeEmpWage();
+        System.out.println("------------Reliance-----------");
+        computeEmpWage("Reliance", 20, 10, 30);
+        System.out.println();
+        System.out.println("------------DMart-----------");
+        computeEmpWage("DMart", 10, 20, 40);
     }
 }
